b61542d6547de9c9d78f9a1d109cf5c24c4e695a
aklimaakter20 <aklimaakter20@gmail.com>
2024-10-07 01:31:59 +0600

    some editing

--- array.js
+++ array.js
@@ -1,42 +1,42 @@
-// function calculateAverage(num) {
-//     let sum = 0;
-//     let avgNumber = 0;
-//     for ( let i = 0; i < num.length; i++){
-//         sum += num[i]
-//     }
-//     return avgNumber = sum / num.length
-// }
-// let array = [12,45,78,23]
-// console.log(calculateAverage(array));
+// 1 no 
+function calculateAverage(num) {
+    let sum = 0;
+    let avgNumber = 0;
+    for ( let i = 0; i < num.length; i++){
+        sum += num[i]
+    }
+    return avgNumber = sum / num.length
+}
+let array = [12,45,78,23]
+console.log(calculateAverage(array));
 
 // 2 no 
-// function isEven(num){
+function isEven(num){
   
-//     if (num % 2 == 0){
-//         return number = " even"
-//     }else{
-//         return number = " odd"
-//     }
-// }
-// console.log(isEven(34));
+    if (num % 2 == 0){
+        return number = " even"
+    }else{
+        return number = " odd"
+    }
+}
+console.log(isEven(34));
 
 // 3 no
 
-// function findMax(array) { 
-//     let i; 
-//     let max = array[0]; 
+function findMax(array) { 
+    let i; 
+    let max = array1[0]; 
 
-//     for (i = 1; i < array.length; i++) {
-//         if (array[i] > max) 
-//             max = array[i]; 
-//     } 
+    for (i = 1; i < array1.length; i++) {
+        if (array1[i] > max) 
+            max = array1[i]; 
+    } 
      
-//     return max; 
-// } 
- 
+    return max; 
+} 
 
-// let array = [4,16,1,9,23];
-// console.log("Largest in given array is " + findMax(array));
+let array1 = [4,16,1,9,23];
+console.log("Largest in given array is " + findMax(array1));
 
 //  4 no
 
@@ -50,14 +50,14 @@ console.log(string1.concat(' ',string2));
  
 // 5 no
 
-// function factorial(num)
-// {
-//     let fact = 1;
-//     for (let i = 1; i <= num; i++)
-//         fact *= i;
-//     return fact;
-// }
+function factorial(num)
+{
+    let fact = 1;
+    for (let i = 1; i <= num; i++)
+        fact *= i;
+    return fact;
+}
 
 
-// let num = 6;
-// console.log("Factorial of " + num + " is " + factorial(6));
+let num = 6;
+console.log("Factorial of " + num + " is " + factorial(6));
