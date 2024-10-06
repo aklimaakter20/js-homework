a79c0a0d403a606536a128f26796f65990a53f4d
aklimaakter20 <aklimaakter20@gmail.com>
2024-10-07 01:27:50 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,63 @@
+// function calculateAverage(num) {
+//     let sum = 0;
+//     let avgNumber = 0;
+//     for ( let i = 0; i < num.length; i++){
+//         sum += num[i]
+//     }
+//     return avgNumber = sum / num.length
+// }
+// let array = [12,45,78,23]
+// console.log(calculateAverage(array));
+
+// 2 no 
+// function isEven(num){
+  
+//     if (num % 2 == 0){
+//         return number = " even"
+//     }else{
+//         return number = " odd"
+//     }
+// }
+// console.log(isEven(34));
+
+// 3 no
+
+// function findMax(array) { 
+//     let i; 
+//     let max = array[0]; 
+
+//     for (i = 1; i < array.length; i++) {
+//         if (array[i] > max) 
+//             max = array[i]; 
+//     } 
+     
+//     return max; 
+// } 
+ 
+
+// let array = [4,16,1,9,23];
+// console.log("Largest in given array is " + findMax(array));
+
+//  4 no
+
+function concatString(string1,string2){
+   
+}
+let string1 = "Frontend";
+let string2 = "Development";
+console.log(string1.concat(' ',string2));
+
+ 
+// 5 no
+
+// function factorial(num)
+// {
+//     let fact = 1;
+//     for (let i = 1; i <= num; i++)
+//         fact *= i;
+//     return fact;
+// }
+
+
+// let num = 6;
+// console.log("Factorial of " + num + " is " + factorial(6));
